a54a2397d47b165ccf60670b453899ba2aa6d405
Mathieu Laisne <mathieulbs@gmail.com>
2022-09-29 11:19:47 +0200

    Updated Doc

--- src/type.ts
+++ src/type.ts
@@ -1,36 +1,68 @@
 import sheet from './character'
 
+/**
+ * Name: Name of the player.
+ * Campaign: All campaigns the player is in as normal player.
+ */
 type player = {
     Name: string,
-    IsAdmin: Boolean,
-    LastConnected: Date,
-    NbMessages: number,
     Campaign: campaign[]
-    ID: number
 }
 
+/**
+ * Extends player to be admin of campaigns
+ */
 type GM = player & {
     AdminOf: campaign[]
 }
 
+/**
+ * Name: Name of the campaign.
+ * Map: List of Maps in the campaign.
+ * CharacterSheets: All character Sheets linked to the campaign.
+ */
 type campaign = {
     Name: string,
     Map: map[],
     CharacterSheets: sheet[]
 }
 
+/**
+ * Plan: Tokens in the lower layer.
+ * Main: Tokens in the middle layer. Players and visible objects, etc.
+ * GM: Tokens in the hidden layer. Visible only to the GM.
+ */
 type map = {
     Plan: token[],
     Main: token[],
     GM: token[]
 }
 
+/**
+ * Image: Image corresponding to the token.
+ * Position: Optional. Position on the map.
+ */
 type token = {
     Image: MediaImage,
-    Position: point2D
+    Position?: point2D
 }
 
 type point2D = {
     x: number,
     y: number
+}
+
+/**
+ * Sender: Player talking.
+ * Content: Text to be displayed.
+ * Type: Either "Chat", "Roll" or "Info". 
+ * Visibility: Either "All", "GM" or "Personal".
+ * Receiver: When "Personal", show only to the Receiver with corresponding id (and GM).
+ */
+type message = {
+    Sender: player,
+    Content: string,
+    Type: string,
+    Visibility: string,
+    Receiver?: player
 }
